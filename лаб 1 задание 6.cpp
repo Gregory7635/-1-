1c19395c75b6c65cb7a2f6536a0792138cbeea20
Гриша Васильев <gregory.vasiliev@gmail.com>
2024-02-23 23:33:01 +0300

    Добавьте файлы проекта.

---
+++ лаб 1 задание 6.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <list>
+using namespace std;
+void duplicateOddValues(list<int>& lst) {
+    list<int>::iterator iter = lst.begin();
+    while (iter != lst.end()) {
+        if (*iter % 2 != 0) {
+            lst.insert(iter, *iter);
+            iter++;
+        }
+        else {
+            iter++;
+        }
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    list<int> lst = { 1, 2, 3, 4, 5 };
+
+    cout << "Исходный список: ";
+    for (const auto& elem : lst) {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+    duplicateOddValues(lst);
+
+    cout << "Преобразованный список: ";
+    for (const auto& elem : lst) {
+        std::cout << elem << " ";
+    }
+    cout << endl;
+    list<int>::iterator iter = lst.begin();
+    int* a = &(*iter);
+    cout << "указатель на первый элемент преобразованного списка " << a;
+
+
+    return 0;
+}
